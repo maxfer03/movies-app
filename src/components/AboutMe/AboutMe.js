21a001d8275bbea133dc7110c5d9fd91cfc0a7ec
Max Fer <sandsferdinand@gmail.com>
2021-07-28 19:57:37 -0300

    updated portfolio link

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -11,7 +11,7 @@ const AboutMe = () => {
                         <img  className = "poster" alt = "Movie Poster" src={myselfImg} />
                         <div>
                         <p>Hi! My name is Máximo Fernández. I'm a Fullstack Developer from Buenos Aires, Argentina. As of 24th of July, 2021, I'm a Teaching Assistant at the Henry Bootcamp, and one month away from completing it.</p>
-                        <p>If you are interested in knowing more about me, feel free to message me to my <a className = "aboutLinks" href = "https://www.linkedin.com/in/m%C3%A1ximo-fern%C3%A1ndez-arenas-540461210/">LinkedIn</a>, and check out my <a className = "aboutLinks" href = "https://personal-portoflio-68gtmhtt3-maxfer03.vercel.app/">Portfolio (WIP)</a> to see my other projects!</p>
+                        <p>If you are interested in knowing more about me, feel free to message me to my <a className = "aboutLinks" href = "https://www.linkedin.com/in/m%C3%A1ximo-fern%C3%A1ndez-arenas-540461210/">LinkedIn</a>, and check out my <a className = "aboutLinks" href = "https://max-fernandez-portfolio.vercel.app/">Portfolio (WIP)</a> to see my other projects!</p>
                         </div>
                         
                         
