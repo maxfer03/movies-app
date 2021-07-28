9240aa1d6f3ab06006b762047d7c2b67f5b30294
Max Fer <sandsferdinand@gmail.com>
2021-07-28 19:32:21 -0300

    fixed typos

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,8 +10,8 @@ const AboutMe = () => {
                     <div className="movieCard">
                         <img  className = "poster" alt = "Movie Poster" src={myselfImg} />
                         <div>
-                        <p>Hi! Im Máximo Fernández. Im a Fullstack Developer from Buenos Aires, Argentina. As of 24th of July, 2021, I'm a Teaching Assistant at the Henry Bootcamp, and one month away from completing it.</p>
-                        <p>If you are interested in knowing more about me, feel free to message me to my <a className = "aboutLinks" href = "https://www.linkedin.com/in/m%C3%A1ximo-fern%C3%A1ndez-arenas-540461210/">LinkedIn</a>, and check out my <a className = "aboutLinks" href = "https://www.google.com/">Portfolio (WIP)</a> to see my other projects!</p>
+                        <p>Hi! My name is Máximo Fernández. I'm a Fullstack Developer from Buenos Aires, Argentina. As of 24th of July, 2021, I'm a Teaching Assistant at the Henry Bootcamp, and one month away from completing it.</p>
+                        <p>If you are interested in knowing more about me, feel free to message me to my <a className = "aboutLinks" href = "https://www.linkedin.com/in/m%C3%A1ximo-fern%C3%A1ndez-arenas-540461210/">LinkedIn</a>, and check out my <a className = "aboutLinks" href = "https://personal-portoflio-68gtmhtt3-maxfer03.vercel.app/">Portfolio (WIP)</a> to see my other projects!</p>
                         </div>
                         
                         
